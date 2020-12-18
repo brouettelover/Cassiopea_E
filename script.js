4908f2814464833256e20367a2e51da2d7f9f9b0
brouettelover <59088394+brouettelover@users.noreply.github.com>
2020-12-18 16:01:44 +0100

    Countdown_functionnal

--- script.js
+++ script.js
@@ -3,33 +3,36 @@ $(document).ready(function(event) {
             var end = 0;
             var fiveSeconds = 5,
             display = $('.Box_starter');
-            console.log(startTimer(fiveSeconds, display));
+            startTimer(fiveSeconds, display);
             $(".Box_starter").html("Ready ?");                  
             $(".Box_starter").css('background-image','none');
 
     });
-    $(".Box_starter").on('keypress',function(event){
-        E_unicode = event.which;
-        if(E_unicode === 101){
-            if( $(".Box_starter").hasClass("colorized")  ){
-                $(".Box_starter").removeClass("colorized"); 
-            }
-            else{
-                $(".Box_starter").addClass("colorized");
-            }
-        }
-    });
+
     function startTimer(duration, display) {
         var timer = duration, seconds;
-        x = setInterval(function () {
+        var x = setInterval(function () {
             seconds = parseInt(timer % 60);
-            display.text(seconds);   
-            if (--timer <= 0) {
+            display.text(seconds);
+            if (--timer < 0) {
                 timer = duration;
-                return timer;
+            }
+            if(seconds == 0){
+                clearInterval(x);
+                display.text("")
+                $(".Box_starter").on('keypress',function(event){
+                    E_unicode = event.which;
+                    if(E_unicode === 101){
+                        if( $(".Box_starter").hasClass("colorized")  ){
+                            $(".Box_starter").removeClass("colorized"); 
+                        }
+                        else{
+                            $(".Box_starter").addClass("colorized");
+                        }
+                    }
+                });            
             }
         }, 1000);
-        return x;
     }
 
 });
